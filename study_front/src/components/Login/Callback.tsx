5deef13b5a9e5be2c784888c6be8dcd2807fa279
Sangkun-svg <gims4752@gmail.com>
2022-07-20 23:50:40 +0900

    faet : set defalut social login

--- study_front/src/components/Login/Callback.tsx
+++ study_front/src/components/Login/Callback.tsx
@@ -1,38 +1,48 @@
 import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 import { useLocation } from "react-router-dom";
 
 export const Callback = () => {
+  // console.log("GITHUB_ClIENT_ID : ", process.env.GITHUB_ClIENT_ID); // dotenv setting err ?
   const GITHUB = {
     ClIENT_ID: "4fb18a74d64e5c2ac9e0",
     ClIENT_SECRETS: "8987cc98f7668ae34b8a4f6e1bb97e6d891f695c",
     CALLBACK_URL: "http://localhost:3000/auth/github/callback",
   };
-  //   const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
   const url = new URL(window.location.href);
   const authorizationCode = url.searchParams.get("code");
-  const data = axios
-    .post("https://github.com/login/oauth/access_token", {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-      data: {
-        client_id: GITHUB.ClIENT_ID,
-        client_secret: GITHUB.ClIENT_SECRETS,
-        code: authorizationCode,
-      },
-    })
-    .then((res) => {
-      console.log("response : ", res);
-    })
-    .catch((err) => {
-      console.log("error : ", err);
-    });
+  const getToken = async () => {
+    try {
+      // const response = await axios.get(
+      //   `${GITHUB.CALLBACK_URL}?code=${authorizationCode}` // expect : access_token=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&scope=&token_type=bearer
+      // );
+      // const { jsonWebToken } = await response.json();
+      // localStorage.setItem('token', jsonWebToken);
+      // navigate("/");
+    } catch (error) {
+      console.error("ERROR : ");
+    }
+  };
+  useEffect(() => {
+    getToken();
+  }, [location, authorizationCode]);
 
   return (
-    <>
-      <h1>im Callback tsx</h1>
-      {/* <p>{location}</p> */}
-      <p></p>
-    </>
+    <div className="authenticate">
+      <div className="container">
+        <h1 className="title">Authenticate</h1>
+        <div className="d-flex justify-content-center loading">
+          <div
+            className="spinner-border text-light spinner-style"
+            role="status"
+          >
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
--- study_front/src/components/Login/Login.tsx
+++ study_front/src/components/Login/Login.tsx
@@ -1,6 +1,7 @@
 export const Login = () => {
   const GITHUB_CALLBACK_URL = "http://localhost:3000/auth/github/callback";
   const GITHUB_LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=4fb18a74d64e5c2ac9e0&redirect_uri=${GITHUB_CALLBACK_URL}`;
+
   const socialLoginHandler = () => {
     window.location.assign(GITHUB_LOGIN_URL);
   };
