fc9a6c890e412375ec923f3666cffacf49284fbb
Sangkun-svg <gims4752@gmail.com>
2022-07-21 16:43:55 +0900

    refactor : change login valid access

--- study_front/src/components/Login/Login.tsx
+++ study_front/src/components/Login/Login.tsx
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Login = () => {
-  // const [isLogin, setIsLogin] = useState(!!localStorage.getItem("token"));
-  const [isLogin, setIsLogin] = useState();
-  console.log(isLogin);
-  const GITHUB_CALLBACK_URL = "http://localhost:3000/auth/github/callback";
-  const GITHUB_LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=4fb18a74d64e5c2ac9e0&redirect_uri=${GITHUB_CALLBACK_URL}`;
+  const [isLogin, setIsLogin] = useState(false);
+  const token = localStorage.getItem("token");
+  const GITHUB_CALLBACK_URL = "http://localhost:3000/auth/github/callback"; // TODO: useing dotenv
+  const GITHUB_LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=4fb18a74d64e5c2ac9e0&redirect_uri=${GITHUB_CALLBACK_URL}`; // TODO: useing dotenv
+
+  useEffect(() => {
+    if (token) setIsLogin(true);
+  }, [token]);
 
   const socialLoginHandler = () => {
     window.location.assign(GITHUB_LOGIN_URL);
   };
+
   return (
     <div className="loginContainer">
-      {/* <a href={GITHUB_LOGIN_URL}>Github으로 로그인</a> -> TODO: 사용하지 않은 이유 서술하기 */}
       {isLogin ? (
         <h1>login 성공</h1>
       ) : (
